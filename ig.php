93a127d76de093c1169697791f0a444236398d54
Frankie Bordone <fbordone@eidigital.com>
2018-12-10 17:16:20 -0500

    change to user endpoint

--- ig.php
+++ ig.php
@@ -6,7 +6,7 @@ require_once('cache.php');
 class InstagramOAuth extends Cache {
 
 	public function get_feed($label, $userid, $accessToken){
-			return parent::get_the_json($label, 'https://api.instagram.com/v1/users/'.$userid.'/media/recent/?access_token='.$accessToken.'&count=20');
+			return parent::get_the_json($label, 'https://api.instagram.com/v1/users/self/media/recent/?access_token='.$accessToken.'&count=20');
 	}
 
 }
